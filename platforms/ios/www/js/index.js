08e8554860c8fd19a700a5634ce54fa78c11a33e
Geoffrey Bauduin <bauduin.geo@gmail.com>
2014-11-03 14:41:12 +0100

    ready to be shipped

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -34,6 +34,7 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        alert("Registering device ...");
         PushNotification.registerDevice("", "4d20e307-9fcf-4cf1-bbb2-a871d03601c1",
             function (pushId) {
                 alert("PushNotification: registered:" + pushId);
